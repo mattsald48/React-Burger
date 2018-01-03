28c5565e0bad3b3019cc32f46982b3dbe4eaabb8
mattsald48 <matt.saldana8@gmail.com>
2018-01-03 16:23:42 -0600

    Added Order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,19 @@ state =  {
 		cheese: 0,
 		meat: 0
 	},
-	totalPrice: 4	
+	totalPrice: 4,
+	purchasable: false	
+}
+
+updatePurchaseState (ingredients) {
+	const sum = Object.keys(ingredients)
+		.map(igKey => {
+			return ingredients[igKey]
+		})
+		.reduce((sum, el) => {
+			return sum+el;
+		}, 0)
+	this.setState({purchasable: sum > 0})
 }
 
 addIngredientHandler = (type) => {
@@ -35,6 +47,7 @@ addIngredientHandler = (type) => {
 	const oldPrice = this.state.totalPrice;
 	const newPrice = oldPrice + priceAddition;
 	this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+	this.updatePurchaseState(updatedIngredients);
 }
 
 removeIngredientHandler = (type) => {
@@ -51,6 +64,7 @@ removeIngredientHandler = (type) => {
 	const oldPrice = this.state.totalPrice;
 	const newPrice = oldPrice - priceDeduction;
 	this.setState({totalPrice: newPrice, ingredients: updatedIngredients})	
+	this.updatePurchaseState(updatedIngredients);
 }
 
 	render () {
@@ -69,6 +83,7 @@ removeIngredientHandler = (type) => {
 					ingredientAdded={this.addIngredientHandler}
 					ingredientRemoved={this.removeIngredientHandler}
 					disabled={disabledInfo}
+					purchasable={this.state.purchasable}
 					price={this.state.totalPrice}/>
 			</Aux>
 		);
